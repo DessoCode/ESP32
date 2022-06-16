5bedc5c9c3c4d96a667a17f997db29491ca48335
DessoCode <46800074+DessoCode@users.noreply.github.com>
2022-06-16 17:41:39 +0200

    Battery check

--- ESP32_E-Ink_Main/ESP32_E-Ink_Main.ino
+++ ESP32_E-Ink_Main/ESP32_E-Ink_Main.ino
@@ -90,6 +90,9 @@ const String url = "https://api.github.com/repos/DessoCode/ESP32/contents/Images
 #define TIME_TO_SLEEP  60        /* Time ESP32 will go to sleep (in seconds) */
 RTC_DATA_ATTR int bootCount = 0;
 
+//Battery 
+const int MAX_ANALOG_VAL = 4095;
+const float MAX_BATTERY_VOLTAGE = 4.2; // Max LiPoly voltage of a 3.7 battery is 4.2
 
 void setup()
 {
@@ -152,7 +155,25 @@ void setup()
   esp_sleep_enable_ext0_wakeup(GPIO_NUM_13,0); //1 = High, 0 = Low  
   //esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
   //Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) + " Seconds");  //Go to sleep now
+
   
+  //Battery
+  // A13 pin is not exposed on Huzzah32 board because it's tied to
+  // measuring voltage level of battery. Note: you must
+  // multiply the analogRead value by 2x to get the true battery
+  // level. See: 
+  // https://learn.adafruit.com/adafruit-huzzah32-esp32-feather/esp32-faq
+  int rawValue = analogRead(A13);
+  // Reference voltage on ESP32 is 1.1V
+  // https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/adc.html#adc-calibration
+  // See also: https://bit.ly/2zFzfMT
+  float voltageLevel = (rawValue / 4095.0) * 2 * 1.1 * 3.3; // calculate voltage level
+  float batteryFraction = voltageLevel / MAX_BATTERY_VOLTAGE;
+  
+  Serial.println((String)"Raw:" + rawValue + " Voltage:" + voltageLevel + "V Percent: " + (batteryFraction * 100) + "%");
+  delay(1000);
+  
+  //Sleepytime
   Serial.println("Going to sleep now");
   delay(1000);
   esp_deep_sleep_start();
